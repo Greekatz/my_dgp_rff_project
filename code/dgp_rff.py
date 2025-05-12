0b73612e2df649e752e68736817e4d1f0478b0f3
Thành Hưng <nguyenthanhung1912@gmail.com>
2025-05-12 22:59:25 +0700

    add new new new

--- code/dgp_rff.py
+++ code/dgp_rff.py
@@ -14,7 +14,8 @@
 
 from __future__ import print_function
 
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
